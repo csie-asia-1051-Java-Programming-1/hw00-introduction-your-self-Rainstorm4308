7b6f41bfe795405a1d389da522f7ddd45412e767
Rainstorm4308 <105021107@live.asia.edu.tw>
2017-02-24 14:57:01 +0800

    hw submit

---
+++ src/hw00.java
@@ -0,0 +1,16 @@
+/*
+* Topic: 第一次作業繳交  自我介紹
+* Date: 2017/02/24
+* Author: 105021107 廖旋宏
+*/
+
+
+public class hw00 {
+
+	public static void main(String[] args) {
+		System.out.println("資工系1A(資電組)");
+		System.out.println("姓名:廖旋宏");
+		System.out.println("學號:105021107");
+	}
+
+}
